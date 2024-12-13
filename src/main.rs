35e4e085ddbda9efda01175213990ace1db406e7
bittu <thedevbittu@gmail.com>
2024-12-13 17:56:31 +0530

    add main and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{env, process::exit};
+
+// convert to chars then rev it and collect it (as it is iter)
+// collect it in string type
+// simple explaination: reverse a string while preserving UTF-8 correctness.
+pub fn reverse_string(str: &str) -> String {
+    str.chars().rev().collect::<String>()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // output:Args { inner: ["target\\debug\\livecli.exe", "arg1", "arg2"] }
+    // we cant access inner as it is private member
+    let args = env::args();
+
+    // _ in Vec<_> ->  auto guess data type
+    // here it is String -> Vec<String>
+    let args: Vec<_> = args.collect();
+
+    // 1st element of args was the program name or exe path
+    let args = &args[1..];
+
+    if args.len() < 1 {
+        println!("You must enter any string");
+        exit(0);
+    }
+
+    for i in args {
+        println!("Original String: {}", i);
+        println!("Reverse String: {}", reverse_string(i));
+
+        println!();
+    }
 }
---
+++ tests/rev_str_test.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+use livecli::*;
+
+#[test]
+fn reverse_string_test() {
+    let strings = HashMap::from([
+        ("test", "tset"),
+        ("madam", "madam"),
+        ("test1", "1tset"),
+        ("bittu", "uttib"),
+        ("dev", "ved"),
+    ]);
+
+    for (key, value) in strings {
+        assert_eq!(reverse_string(key), value);
+    }
+}
